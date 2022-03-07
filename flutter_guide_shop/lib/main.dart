34d7e488558bee5dfa44cc39c0ba70dd42c8b04c
Konrad Kudlak <koqud.dev@gmail.com>
2022-03-07 21:49:04 +0100

    FINISHED SHOP APP

--- flutter_guide_shop/lib/main.dart
+++ flutter_guide_shop/lib/main.dart
@@ -13,6 +13,7 @@ import './screens/product_detail_screen.dart';
 import './providers/products.dart';
 import './screens/edit_product_screen.dart';
 import './screens/auth_screen.dart';
+import './helpers/custom_route.dart';
 
 void main() => runApp(MyApp());
 
@@ -44,9 +45,15 @@ class MyApp extends StatelessWidget {
         builder: (ctx, auth, _) => MaterialApp(
           title: 'MyShop',
           theme: ThemeData(
-              primarySwatch: Colors.purple,
-              accentColor: Colors.deepOrange,
-              fontFamily: 'Lato'),
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato',
+            pageTransitionsTheme: PageTransitionsTheme(builders: {
+              //changing animation of all page transitions in app
+              TargetPlatform.android: CustomPageTransitionBuilder(),
+              TargetPlatform.iOS: CustomPageTransitionBuilder(),
+            }),
+          ),
           home: auth.isAuth
               ? ProductOverviewScreen()
               : FutureBuilder(
--- flutter_guide_shop/lib/screens/product_detail_screen.dart
+++ flutter_guide_shop/lib/screens/product_detail_screen.dart
@@ -15,36 +15,46 @@ class ProductDetailScreem extends StatelessWidget {
     final loadedProduct = Provider.of<Products>(context, listen: false).findById(
         productId); //because of listen false build will not be rebuilded when sth is changing in app
     return Scaffold(
-      appBar: AppBar(
-        title: Text(loadedProduct.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              child: Image.network(loadedProduct.imageUrl, fit: BoxFit.cover),
+      // appBar: AppBar(
+      //   title: Text(loadedProduct.title),
+      // ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(loadedProduct.title),
+              background: Hero(
+                  tag: loadedProduct.id,
+                  child:
+                      Image.network(loadedProduct.imageUrl, fit: BoxFit.cover)),
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              '\$${loadedProduct.price}',
-              style: TextStyle(color: Colors.grey, fontSize: 20),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                width: double.infinity,
-                child: Text(
-                  loadedProduct.description,
-                  textAlign: TextAlign.center,
-                  softWrap: true,
-                ))
-          ],
-        ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                '\$${loadedProduct.price}',
+                style: TextStyle(color: Colors.grey, fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  width: double.infinity,
+                  child: Text(
+                    loadedProduct.description,
+                    textAlign: TextAlign.center,
+                    softWrap: true,
+                  ))
+            ]),
+          ),
+        ],
       ),
     );
   }
